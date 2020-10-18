1bab95d1e58291ea83a4fb98244394634e20753b
AdrianMValencia <adrianmechatovalencia2@gmail.com>
2020-10-18 18:50:38 -0500

    Implementación de la plantilla AdminLTE - Login - Sweetalert2 | 18/10/2020

---
+++ view/user/list_user_view.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Usuarios</h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+            </div>
+        </div>
+        <div class="card-body">
+            Lista de Usuarios
+        </div>
+    </div>
+</div>
